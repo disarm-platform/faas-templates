68ce7d9cbce113eda51965caea884c4e7a51a67b
Jonathan Smith <jonathan@peoplesized.com>
2019-10-30 11:06:06 +0200

    instructions

--- template/python-geospatial/function/preprocess_params.py
+++ template/python-geospatial/function/preprocess_params.py
@@ -2,4 +2,4 @@
 # Throw an error any time a test for a param fails.
 # Otherwise, return nothing
 def preprocess(params: dict):
-    raise ValueError('Need to implement `preprocess` steps in function/preprocess_params.py')
+    raise ValueError('Need to implement `preprocess` steps in function/preprocess_params.py or replace with `pass`')
